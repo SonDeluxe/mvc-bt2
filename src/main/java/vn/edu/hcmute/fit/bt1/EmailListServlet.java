3e42fc9debf77a4dc6560bd9e9ed9d574b37cdd0
son <nhokvh00@gmail.com>
2025-08-22 21:12:11 +0700

    up

---
+++ src/main/java/vn/edu/hcmute/fit/bt1/EmailListServlet.java
@@ -0,0 +1,44 @@
+package vn.edu.hcmute.fit.bt1;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class EmailListServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = "/index.html";
+
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";
+        }
+
+        if (action.equals("join")) {
+            url = "/index.html";
+        } else if (action.equals("add")) {
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            User user = new User(firstName, lastName, email);
+
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";
+        }
+
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
